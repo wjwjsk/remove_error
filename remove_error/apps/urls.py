3dc5094f365dd3ea6bcf620393b0adae2215646a
kimziho <rlawlgh1027@naver.com>
2023-10-18 17:51:06 +0900

    Feat: db 데이터 불러오기

--- remove_error/apps/urls.py
+++ remove_error/apps/urls.py
@@ -2,5 +2,6 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-]   
+    path("", views.main, name="main"),
+    # path('item/<int:item_id>/', views.item_detail, name='item_detail'),
+]
--- remove_error/apps/views.py
+++ remove_error/apps/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Items, Category
 
-# Create your views here.
-def index(request):
-    return render(request, 'index.html')
+
+# 메인 페이지
+def main(request):
+    items = Items.objects.all()
+    categories = Category.objects.all()
+
+    context = {
+        "items": items,
+        "categories": categories,
+    }
+    return render(request, "index.html", context)
+
+
+# 상세 페이지
+# def item_detail(request, item_id):
+#     item = get_object_or_404(Items, pk=item_id)
+#     context = {
+#         'item': item,
+#     }
+
+#     return render(request, 'item.html', context)

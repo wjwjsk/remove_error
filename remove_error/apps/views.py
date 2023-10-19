078349e4b0645c47d8439cdcf0412c2ac2b4ac6a
park <wjwjsk12345@gmail.com>
2023-10-19 16:46:13 +0900

    크롤링 업데이트

--- remove_error/apps/views.py
+++ remove_error/apps/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
-
+from .crawl import fm_crawling_function
+from .models import Items, Category
 # Create your views here.
 def index(request):
+    result = fm_crawling_function()
+# 전치 수행
+    transposed_result = list(zip(*result))
+
+    for column in transposed_result:
+        for data in column:
+            # 전달받은 데이터를 ResultModel에 매핑하여 저장합니다.
+            result_model = Items(
+                board_url=data['board_url'],
+                item_name=data['item_name'],
+                end_url=data['end_url'],
+                clr_update_time=data['clr_update_time'],
+                board_price=data['board_price'],
+                board_description=data['board_description'],
+                delivery_price=data['delivery_price'],
+                is_end_deal=data['is_end_deal'],
+            )
+            result_model.save()
+    
     return render(request, 'index.html')

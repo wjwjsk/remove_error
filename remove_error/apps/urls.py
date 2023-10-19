ac912e3e28bd0d5486adf532892a61a00a479270
kimziho <rlawlgh1027@naver.com>
2023-10-19 13:27:23 +0900

    Feat: 카테고리 필터링 기능 추가

--- remove_error/apps/urls.py
+++ remove_error/apps/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.main, name="main"),
     # path('item/<int:item_id>/', views.item_detail, name='item_detail'),
+    path("items/<int:category_id>/", views.item_list_by_category, name="item_list_by_category"),
 ]
--- remove_error/apps/views.py
+++ remove_error/apps/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Items, Category
+from django.http import JsonResponse
 
 
 # 메인 페이지
@@ -22,3 +23,15 @@ def main(request):
 #     }
 
 #     return render(request, 'item.html', context)
+
+
+def item_list_by_category(request, category_id):
+    # 선택한 카테고리에 해당하는 아이템들을 필터링합니다.
+    items = Items.objects.filter(category=category_id)
+    categories = Category.objects.all()
+
+    context = {
+        "items": items,
+        "categories": categories,
+    }
+    return render(request, "index.html", context)

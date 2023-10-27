875059e3f40d9fac5423f4f7c2eeeb74d3bccadd
juni9797 <wnsgml2882@naver.com>
2023-10-27 15:12:25 +0900

    feat : 프로젝트 url 추가

--- remove_error/remove_error/urls.py
+++ remove_error/remove_error/urls.py
@@ -19,5 +19,9 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include('social_django.urls')),
     path("", include("apps.urls")),
+    
+    path('accounts/', include('allauth.urls')),
+
 ]

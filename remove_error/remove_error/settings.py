e9a92680073b96089b7d306163c7c766991c603a
jhyeokqq <jinmb4136@gmail.com>
2023-10-31 08:39:56 +0900

    modify:login

--- remove_error/remove_error/settings.py
+++ remove_error/remove_error/settings.py
@@ -179,3 +179,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTHENTICATION_ERROR_MESSAGES = {
+    'invalid_login': "아이디 또는 비밀번호가 올바르지 않습니다. 다시 시도해주세요.",
+}

3ab3268b3350c2b5ba2df5d42e6736bde6660d96
park <wjwjsk12345@gmail.com>
2023-11-03 02:41:59 +0900

    파일변경

--- remove_error/remove_error/settings.py
+++ remove_error/remove_error/settings.py
@@ -170,7 +170,7 @@ LOGOUT_REDIRECT_URL = "/"
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = "static/"
+STATIC_ROOT = "../static/"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
